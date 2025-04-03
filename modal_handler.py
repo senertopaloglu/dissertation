9ff95ad852a9496740abef6008babe65b122b533
senertopaloglu <sbtopaloglu1@gmail.com>
2025-04-03 20:32:00 +0100

    refactor: remove unused methods from modal_handler

--- modal_handler.py
+++ modal_handler.py
@@ -35,25 +35,6 @@ vol = modal.Volume.from_name("sam_2_medical_3d")
 app = modal.App("example-3")
 
 
-
-def show_mask(mask, ax, obj_id=None, random_color=False):
-    if random_color:
-        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
-    else:
-        cmap = plt.get_cmap("tab10")
-        cmap_idx = 0 if obj_id is None else obj_id
-        color = np.array([*cmap(cmap_idx)[:3], 0.6])
-    h, w = mask.shape[-2:]
-    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
-    ax.imshow(mask_image)
-
-
-def show_points(coords, labels, ax, marker_size=200):
-    pos_points = coords[labels==1]
-    neg_points = coords[labels==0]
-    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-
 @app.function(gpu="L4", image=image, volumes={"/root/temp":vol}, timeout=1000, mounts=[])
 def do_some_magic(points, frame_idx, foldername, multi_resolution, is_first, is_final):
     import subprocess
