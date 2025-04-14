9ef3313f0bb815efdb045dd81238510f6a4a19cf
senertopaloglu <sbtopaloglu1@gmail.com>
2025-04-14 16:50:19 +0100

    fix: import metrica in segmentation results script

--- test/segmentation_results_script.py
+++ test/segmentation_results_script.py
@@ -8,7 +8,7 @@ import numpy as np
 from PIL import Image
 import glob
 import cv2
-from test.metrics import dice_coefficient, new_assd, DICE
+from metrics import dice_coefficient, new_assd, DICE
 
 def load_mask(filepath: str) -> np.ndarray:
     """
